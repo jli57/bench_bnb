7858dca75f73bfda3dd84e0fdd1eb94b9f4a9f8b
Jingna Li <jingna13@gmail.com>
2018-10-08 11:51:43 -0700

    Add Authroute

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -2,12 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { signup, login, logout } from './util/api';
 import configureStore from './store/store';
-import Root from './components/root'; 
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  const store = configureStore();
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
+  let store;
+
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: window.currentUser
+      },
+      session: { id: Object.keys(window.currentUser)[0] }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+
   ReactDOM.render(<Root store={store}/>, root);
 });
--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import GreetingContainer from './greeting/greeting_container';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
+import { AuthRoute } from '../util/route_util';
 
-import { Route } from 'react-router-dom'; 
+import { Route } from 'react-router-dom';
 
 const App = () => (
   <div>
     <h1>Bench BnB</h1>
     <GreetingContainer />
-    <Route path="/login" component={LoginFormContainer} />
-    <Route path="/signup" component={SignupFormContainer} />
+    <AuthRoute path="/login" component={LoginFormContainer} />
+    <AuthRoute path="/signup" component={SignupFormContainer} />
   </div>
 );
 
